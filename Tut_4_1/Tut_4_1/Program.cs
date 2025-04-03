65ea4fb25e225faedeb3c11cf76367b27ff8dfef
Shavana <127197288+ShavanaM@users.noreply.github.com>
2025-04-03 18:30:37 +0200

    Add files via upload

---
+++ Tut_4_1/Tut_4_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNum1, iNum2;
+            double dAvg;
+
+            iNum1 = 34;
+            iNum2 = 22;
+
+            dAvg = (iNum1 + iNum2) / 2;
+
+            Console.WriteLine("The numbers are "+iNum1+" and "+ iNum2);
+            Console.WriteLine(dAvg+" is the average");
+
+            Console.ReadLine();
+
+
+
+        
+
+        }
+    }
+}
